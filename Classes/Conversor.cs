ab0d493086c585fd9a9cbeb02f35ff789a9c5a2e
Joao Nascimento <jhonyzito4gb@gmail.com>
2020-12-14 21:33:01 -0300

    Dolar, Euro, Iene e menu feito

--- Classes/Conversor.cs
+++ Classes/Conversor.cs
@@ -1,8 +1,15 @@
-namespace DolarEuroLibra.Classes
+namespace DolarEuroIene.Classes
 {
     public static class Conversor
     {
+
+        public static string respostaMenu;
+
+        public static float respostaMetodos;
         private static float cotacaoDolar = 5.11f;
+        private static float cotacaoEuro = 6.21f;
+
+        private static float cotacaoIene = 0.049f;
 
         public static float RealParaDolar(float valorRS)
         {
@@ -12,6 +19,26 @@ namespace DolarEuroLibra.Classes
         public static float DolarParaReal(float valorUS)
         {
             return cotacaoDolar * valorUS; 
+        }
+
+        public static float RealParaEuro(float valorRS)
+        {
+            return valorRS / cotacaoEuro;
+        }
+
+        public static float EuroParaReal(float valorEU)
+        {
+            return valorEU * cotacaoEuro;
+        }
+
+        public static float IeneParaReal(float valorRS)
+        {
+            return valorRS / cotacaoIene;
+        }
+
+        public static float RealParaIene(float valorYEN)
+        {
+            return valorYEN * cotacaoIene;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,69 @@
 ﻿using System;
-using DolarEuroLibra.Classes;
-namespace Dolar__Euro__Libra
+using DolarEuroIene.Classes;
+namespace Dolar__Euro__Iene
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Conversor.DolarParaReal(1));
-            Console.WriteLine(Conversor.RealParaDolar(3));
+            Console.WriteLine("----------------------------------------");
+            
+            Console.WriteLine("Bem vindo a Johnny's today's economy.");
+            
+            Console.WriteLine("----------------------------------------");
+            
+            do
+            {
+                
+                Console.WriteLine("Qual moeda você deseja comparar com Real?");
+                Console.WriteLine("[1] - Dolar.");
+                Console.WriteLine("[2] - Euro.");
+                Console.WriteLine("[3] - Iene.");
+                Console.WriteLine("[4] - Sair do Site.");
+                Conversor.respostaMenu = Console.ReadLine();
+
+                switch (Conversor.respostaMenu)
+                {
+                    case "1":
+                        Console.WriteLine("Digite o valor que deseja calcular de real para dolar:");
+                        Console.WriteLine(Conversor.RealParaDolar(Conversor.respostaMetodos = float.Parse(Console.ReadLine( ) ) ) );
+                        Console.WriteLine("Agora digite um valor de Dolar para Real:");
+                        Console.WriteLine(Conversor.DolarParaReal(Conversor.respostaMetodos = float.Parse(Console.ReadLine( ) ) ) );
+                        Console.WriteLine("Retornando ao Menu.");
+                        break;
+                    case "2":
+                        Console.WriteLine("Digite o valor que deseja calcular de Real para dolar:");
+                        Console.WriteLine(Conversor.RealParaEuro(Conversor.respostaMetodos = float.Parse(Console.ReadLine() ) ) );
+                        Console.WriteLine("Agora digite um valor para converter de Euro para Real:");
+                        Console.WriteLine(Conversor.EuroParaReal(Conversor.respostaMetodos = float.Parse(Console.ReadLine() ) ) );
+                        Console.WriteLine("Retornando ao Menu.");
+                        break;
+                    case "3":
+                        Console.WriteLine("Digite o valor que deseja calcular de real para Iene:");
+                        Console.WriteLine(Conversor.IeneParaReal(Conversor.respostaMetodos = float.Parse(Console.ReadLine( ) ) ) );
+                        Console.WriteLine("Agora digite um valor de Iene para Real:");
+                        Console.WriteLine(Conversor.RealParaIene(Conversor.respostaMetodos = float.Parse(Console.ReadLine( ) ) ) );
+                        Console.WriteLine("Retornando ao Menu.");
+                        break;    
+                    default:
+                        Console.WriteLine("Saindo do Site.");
+                        break;
+                        
+                }
+                System.Threading.Thread.Sleep(5000);
+                Console.Clear();
+            }while (Conversor.respostaMenu != "4");
+            
+
+            // //Dolar
+            // Console.WriteLine(Conversor.DolarParaReal(1));
+            // Console.WriteLine(Conversor.RealParaDolar(5));
+            // //Euro
+            // Console.WriteLine(Conversor.EuroParaReal(1));
+            // Console.WriteLine(Conversor.RealParaEuro(6));
+            // //Iene
+            // Console.WriteLine(Conversor.IeneParaReal(1));
+            // Console.WriteLine(Conversor.RealParaIene(2));
         }
     }
 }
